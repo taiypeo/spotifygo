973a0fcea4d2619544e3381369a8373f07edf64b
QwertygidQ <Qwertygid@gmail.com>
2020-08-18 23:50:22 +0300

    Add TrackModeType

--- apiobjects/audioFeatures.go
+++ apiobjects/audioFeatures.go
@@ -1,30 +1,9 @@
 package apiobjects
 
 import (
-	"errors"
-
 	"github.com/taiypeo/spotifygo"
 	"github.com/taiypeo/spotifygo/apierrors"
 )
-
-// ConvertibleBool is a custom bool type that we define an UnmarshalJSON method for,
-// so we can convert integers to booleans (0 -> false, 1 -> true).
-type ConvertibleBool bool
-
-// UnmarshalJSON is the method we define, so that ConvertibleBool implements the Unmarshaler
-// interface.
-func (convertibleBool *ConvertibleBool) UnmarshalJSON(data []byte) error {
-	strData := string(data)
-	if strData == "0" {
-		*convertibleBool = false
-	} else if strData == "1" {
-		*convertibleBool = true
-	} else {
-		return errors.New("Invalid input to UnmarshalJSON for ConvertibleBool")
-	}
-
-	return nil
-}
 
 // TrackKeyType represents a key the track is in. It is equivalent to int64.
 type TrackKeyType int64
@@ -69,27 +48,47 @@ func (keyType TrackKeyType) String() (string, apierrors.TypedError) {
 	return strKeyType, nil
 }
 
+// TrackModeType represents the modality of the track. It is equivalent to int64.
+type TrackModeType int64
+
+const (
+	// MinorModeType is the minor modality.
+	MinorModeType TrackModeType = iota
+	// MajorModeType is the major modality.
+	MajorModeType
+)
+
+func (keyType TrackModeType) String() (string, apierrors.TypedError) {
+	if keyType == MinorModeType {
+		return "Minor", nil
+	} else if keyType == MajorModeType {
+		return "Major", nil
+	}
+
+	return "", apierrors.NewBasicErrorFromString("Unknown TrackModeType")
+}
+
 // AudioFeatures represents an audio features object
 // in the Spotify API Object model.
 type AudioFeatures struct {
-	Acousticness     float64         `json:"acousticness"`
-	AnalysisURL      string          `json:"analysis_url"`
-	Danceability     float64         `json:"danceability"`
-	DurationMS       int64           `json:"duration_ms"`
-	Energy           float64         `json:"energy"`
-	ID               string          `json:"id"`
-	Instrumentalness float64         `json:"instrumentalness"`
-	Key              TrackKeyType    `json:"key"`
-	Liveness         float64         `json:"liveness"`
-	Loudness         float64         `json:"loudness"`
-	Mode             ConvertibleBool `json:"mode"`
-	Speechiness      float64         `json:"speechiness"`
-	Tempo            float64         `json:"tempo"`
-	TimeSignature    int64           `json:"time_signature"`
-	TrackHref        string          `json:"track_href"`
-	Type             string          `json:"type"`
-	URI              string          `json:"URI"`
-	Valence          float64         `json:"valence"`
+	Acousticness     float64       `json:"acousticness"`
+	AnalysisURL      string        `json:"analysis_url"`
+	Danceability     float64       `json:"danceability"`
+	DurationMS       int64         `json:"duration_ms"`
+	Energy           float64       `json:"energy"`
+	ID               string        `json:"id"`
+	Instrumentalness float64       `json:"instrumentalness"`
+	Key              TrackKeyType  `json:"key"`
+	Liveness         float64       `json:"liveness"`
+	Loudness         float64       `json:"loudness"`
+	Mode             TrackModeType `json:"mode"`
+	Speechiness      float64       `json:"speechiness"`
+	Tempo            float64       `json:"tempo"`
+	TimeSignature    int64         `json:"time_signature"`
+	TrackHref        string        `json:"track_href"`
+	Type             string        `json:"type"`
+	URI              string        `json:"URI"`
+	Valence          float64       `json:"valence"`
 }
 
 // Validate returns a TypedError if a AudioFeatures struct is incorrect.
@@ -126,6 +125,10 @@ func (features AudioFeatures) Validate() apierrors.TypedError {
 
 	if features.Key < CKeyType || features.Key > BKeyType {
 		return apierrors.NewBasicErrorFromString("Key is invalid in AudioFeatures")
+	}
+
+	if features.Mode != MinorModeType && features.Mode != MajorModeType {
+		return apierrors.NewBasicErrorFromString("Mode is invalid in AudioFeatures")
 	}
 
 	if features.Liveness < 0 || features.Liveness > 1 {
